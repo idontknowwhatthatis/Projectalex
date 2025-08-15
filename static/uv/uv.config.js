71e28d22d0a79b803280847a269b30b8c5bde4cb
idontknowwhatthatis <77902299+idontknowwhatthatis@users.noreply.github.com>
2025-08-15 08:43:04 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,11 +1,10 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-	prefix: "/students/lesson/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/uv/uv.handler.js",
-	client: "/uv/uv.client.js",
-	bundle: "/uv/uv.bundle.js",
-	config: "/uv/uv.config.js",
-	sw: "/uv/uv.sw.js",
+    prefix: '/service/',
+    bare: 'https://incog.dev/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
 };
